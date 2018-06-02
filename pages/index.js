4a3aac33a63f6e7c96d23f60d5dafa03e45df7de
tyobaskara <prasetyaajibaskara@gmail.com>
2018-06-02 22:32:40 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,6 @@ export default class Index extends React.Component {
             <Layout>
                 <Head>
                     <title>Shopee</title>
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <meta name="author" content="Prasetya Aji Baskara" />
-                    <link rel="shortcut icon" type="image/x-icon" href="../static/images/favicon.ico" />
-                    <meta name="robots" content="noindex, nofollow" />
                 </Head>
 
                 <BannerHome />
